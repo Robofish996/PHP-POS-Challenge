028fa2063ee6917c1055139477d8b5d2fdd8ad60
Matthew Riddle <Matthew.A.Riddle@outlook.com>
2023-06-29 19:29:09 +0200

    Adding images

--- data/data.php
+++ data/data.php
@@ -4,46 +4,55 @@ $items = [
     [
         "name" => "LG TV",
         "price" => 100,
-        "barcode" => 18367
+        "barcode" => 18367,
+        "image" => "./static/img/products/lgTV.jpg",
     ], 
     [
         "name" => "Defy Kettle",
         "price" => 50,
-        "barcode" => 23111
+        "barcode" => 23111,
+        "image" => "./static/img/products/defyKettle.jpg",
     ],
     [
         "name" => "HiSense Headset",
         "price" => 80,
-        "barcode" => 14837
+        "barcode" => 14837,
+        "image" => "./static/img/products/hisenseHeadset.jpg",
     ],     
     [
         "name" => "Samsung Microwave",
         "price" => 100,
-        "barcode" => 30921
+        "barcode" => 30921,
+        "image" => "./static/img/products/samsungMicrowave.jpg",
     ],     
     [
         "name" => "JVC Home Theatre System",
         "price" => 70,
-        "barcode" => 83120
+        "barcode" => 83120,
+        "image" => "./static/img/products/jvcHomeTheatreSystem.jpg",
     ],
     [
         "name" => "Radio JVC",
         "price" => 20,
-        "barcode" => 82180
+        "barcode" => 82180,
+        "image"=> "./static/img/products/jvcradio.jpg",
     ],
     [
         "name" => "Samsung Washing Machine",
         "price" => 270,
-        "barcode" => 30299
+        "barcode" => 30299,
+        "image"=> "./static/img/products/samsungWashingmachine.jpg",
     ],
     [
         "name" => "Telefunken TV",
         "price" => 70,
-        "barcode" => 31204
+        "barcode" => 31204,
+        "image" => "./static/img/products/telefunkenTV.jpg",
     ],
     [
         "name" => "Philips Air Fryer",
         "price" => 90,
-        "barcode" => 13204
+        "barcode" => 13204,
+        "image" => "./static/img/products/airFryer.jpg",
     ]
 ];
